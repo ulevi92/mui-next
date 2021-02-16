362d2a2c4c063b374a6a67b3327a7652b5b89e10
Uria <ulevi92@gmail.com>
2021-02-16 20:32:55 +0200

    Can't work with stopPropagation with Next.js may open a new React app project

--- components/navbar/Dropdown.tsx
+++ components/navbar/Dropdown.tsx
@@ -1,47 +1,38 @@
-import { FC, useRef, useState } from "react";
+import { FC, useRef, useState, useEffect } from "react";
 import Link from "next/link";
 
 import { Button } from "@material-ui/core";
 
 import { dropdownStyles } from "../../theme/theme";
 
-interface DropdownProps {
-  path?: string[];
-}
-
-const Dropdown: FC<DropdownProps> = ({ path }) => {
-  const listRef = useRef<HTMLDivElement>();
-
+const Dropdown: FC = () => {
   const [active, setActive] = useState(false);
 
-  const classes = dropdownStyles();
+  const menuRef = useRef<HTMLDivElement>(null);
 
-  const dropdownList = ["account", "logout"];
-
-  const renderList = dropdownList.map((item, i) => {
-    return (
-      <Link key={i} href={`/${item}`}>
-        <a className={(classes.dropdownItem, active ? "active" : "")}>{item}</a>
-      </Link>
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        setActive(false);
+      },
+      { capture: true }
     );
-  });
+  }, []);
 
-  const onDropdownClick = () => {
+  const onButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     setActive(!active);
-    listRef.current.classList.toggle("active");
   };
 
   return (
-    <>
-      <Button onClick={onDropdownClick}>Dropdown</Button>
-      <div ref={listRef} className={classes.dropdownList}></div>
-    </>
+    <div>
+      <button onClick={onButtonClick}>Click</button>
+
+      <h1>Dropdown</h1>
+      <h1>{active ? "true" : "false"}</h1>
+    </div>
   );
 };
-export default Dropdown;
 
-{
-  /* <Link key={item} href={`/${path[i]}`}>
-<a>{item}</a>
-</Link> */
-}
+export default Dropdown;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,5 @@
-import { Button, makeStyles, withStyles } from "@material-ui/core";
-
-const ColorButton = withStyles((theme) => ({
-  root: {
-    background: theme.palette.primary.main,
-    "&:hover": {
-      background: "#a15a15",
-    },
-  },
-}))(Button);
-
-const useStyles = makeStyles({
-  btn: {
-    borderRadius: 0,
-  },
-});
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const classes = useStyles();
-
-  return (
-    <>
-      <ColorButton
-        variant="contained"
-        color="primary"
-        style={{ marginRight: "10px" }}
-      >
-        Click
-      </ColorButton>
-      <Button variant="contained" color="primary" className={classes.btn}>
-        Click
-      </Button>
-    </>
-  );
+  return <></>;
 }
