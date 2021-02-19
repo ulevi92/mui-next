c5356848bb2ac13bb09662f54dea81d2265665fe
Uria <ulevi92@gmail.com>
2021-02-19 13:45:09 +0200

    .

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,64 +1,64 @@
 import Link from "next/link";
 
-import { useAboutStyles } from "../theme/theme";
-import { Grid, SvgIcon, Typography } from "@material-ui/core";
-import CheckIcon from "@material-ui/icons/Check";
+// import { useAboutStyles } from "../theme/theme";
+// import { Grid, SvgIcon, Typography } from "@material-ui/core";
+// import CheckIcon from "@material-ui/icons/Check";
 
-const About = () => {
-  const classes = useAboutStyles();
+// const About = () => {
+//   const classes = useAboutStyles();
 
-  const renderCreatedList = [
-    "React.js",
-    "Next.js",
-    "Typescript",
-    "Material-Ui",
-    "Firebase",
-  ].map((item, i) => <Typography key={i}>{item}</Typography>);
+//   const renderCreatedList = [
+//     "React.js",
+//     "Next.js",
+//     "Typescript",
+//     "Material-Ui",
+//     "Firebase",
+//   ].map((item, i) => <Typography key={i}>{item}</Typography>);
 
-  const renderIconList = [0, 0, 0, 0, 0].map((item, i) => (
-    <SvgIcon key={i} color="primary">
-      <CheckIcon />
-    </SvgIcon>
-  ));
+//   const renderIconList = [0, 0, 0, 0, 0].map((item, i) => (
+//     <SvgIcon key={i} color="primary">
+//       <CheckIcon />
+//     </SvgIcon>
+//   ));
 
-  return (
-    <>
-      <Typography className={classes.h1} variant="h4">
-        Created By Uria Levi With:
-      </Typography>
+//   return (
+//     <>
+//       <Typography className={classes.h1} variant="h4">
+//         Created By Uria Levi With:
+//       </Typography>
 
-      <Grid item container>
-        <Grid
-          item
-          xs={1}
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          {renderIconList}
-        </Grid>
-        <Grid
-          item
-          xs={11}
-          container
-          direction="column"
-          justify="center"
-          alignItems="flex-start"
-        >
-          {renderCreatedList}
-        </Grid>
-      </Grid>
-      <Typography className={classes.h1} variant="h4">
-        <a
-          target="_blank"
-          href="https://www.upwork.com/o/profiles/users/~0184fa6042c85fa083/"
-        >
-          Upwork profile
-        </a>
-      </Typography>
-    </>
-  );
-};
+//       <Grid item container>
+//         <Grid
+//           item
+//           xs={1}
+//           container
+//           direction="column"
+//           justify="center"
+//           alignItems="center"
+//         >
+//           {renderIconList}
+//         </Grid>
+//         <Grid
+//           item
+//           xs={11}
+//           container
+//           direction="column"
+//           justify="center"
+//           alignItems="flex-start"
+//         >
+//           {renderCreatedList}
+//         </Grid>
+//       </Grid>
+//       <Typography className={classes.h1} variant="h4">
+//         <a
+//           target="_blank"
+//           href="https://www.upwork.com/o/profiles/users/~0184fa6042c85fa083/"
+//         >
+//           Upwork profile
+//         </a>
+//       </Typography>
+//     </>
+//   );
+// };
 
-export default About;
+// export default About;
